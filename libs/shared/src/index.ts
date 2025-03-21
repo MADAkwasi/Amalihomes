2cb1007d1d913999ab45d9a02287050d2dbdf180
quavo19 <donald.akite@amalitech.com>
2025-03-21 11:02:35 +0000

    Fix exports

--- libs/shared/src/index.ts
+++ libs/shared/src/index.ts
@@ -1,3 +1,2 @@
 export * from './components/button/button.component';
-export * from './components/dynamamic-text/dynamic-text.component';
 export * from './components/text-input/text-input.component';
