4ec67ed2bc11d8551c34d2865a9c51c616d8132e
quavo19 <donald.akite@amalitech.com>
2025-03-26 15:45:28 +0000

    Change naming and signal selection

--- libs/shared/src/components/select-input/select-input.component.ts
+++ libs/shared/src/components/select-input/select-input.component.ts
@@ -1,7 +1,8 @@
-import { Component, computed, effect, input, signal } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule, FormControl } from '@angular/forms';
 import { LucideAngularModule, ChevronDown } from 'lucide-angular';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'lib-select-input',
@@ -16,22 +17,13 @@ export class SelectInputComponent {
   disabled = input(false);
   control = input<FormControl>(new FormControl(''));
   label = input('');
-  cssClass = input('');
   arrowIcon = ChevronDown;
   isOpen = false;
-  private controlValue = signal<string>('');
-  constructor() {
-    effect((onCleanup) => {
-      const ctrl = this.control();
-      this.controlValue.set(ctrl.value);
-      const sub = ctrl.valueChanges.subscribe((value) => {
-        this.controlValue.set(value);
-      });
-      onCleanup(() => sub.unsubscribe());
-    });
-  }
-  computedLabel = computed(() => {
-    const selectedOption = this.options().find((opt) => opt.value === this.controlValue());
+  controlValue = toSignal(this.control().valueChanges, {
+    initialValue: this.control().value,
+  });
+  selectedLabel = computed(() => {
+    const selectedOption = this.options().find(({ value }) => value === this.controlValue());
     return selectedOption ? selectedOption.label : this.placeholder();
   });
   toggleDropdown(): void {
