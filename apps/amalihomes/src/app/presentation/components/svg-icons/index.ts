b975d49767154e92b155c658629f98ab513b050a
quavo19 <donald.akite@amalitech.com>
2025-03-27 10:15:29 +0000

    Add information svg icon

--- apps/amalihomes/src/app/presentation/components/svg-icons/index.ts
+++ apps/amalihomes/src/app/presentation/components/svg-icons/index.ts
@@ -2,3 +2,4 @@ export * from './home-award-icon/home-award-icon.component';
 export * from './home-couch-icon/home-couch-icon.component';
 export * from './home-money-bag-icon/home-money-bag-icon.component';
 export * from './home-truck-icon/home-truck-icon.component';
+export * from './information-circle-icon/information-circle-icon.component';
---
+++ apps/amalihomes/src/app/presentation/components/svg-icons/information-circle-icon/information-circle-icon.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-information-circle-icon',
+  imports: [CommonModule],
+  templateUrl: './information-circle-icon.component.html',
+})
+export class InformationCircleIconComponent {}
