dafd542bce964beb6e672fda58b7ef14dbd6efc9
quavo19 <donald.akite@amalitech.com>
2025-03-28 09:42:17 +0000

    Add pages form unconstructee routes

--- apps/amalihomes/src/app/app.routes.ts
+++ apps/amalihomes/src/app/app.routes.ts
@@ -6,6 +6,22 @@ export const appRoutes: Route[] = [
     loadComponent: () => import('./presentation/pages/home/home.component').then((Default) => Default.HomeComponent),
   },
   {
+    path: 'shop',
+    loadComponent: () => import('./presentation/pages/shop/shop.component').then((Default) => Default.ShopComponent),
+  },
+  {
+    path: 'about',
+    loadComponent: () => import('./presentation/pages/about/about.component').then((Default) => Default.AboutComponent),
+  },
+  {
+    path: 'faqs',
+    loadComponent: () => import('./presentation/pages/faq/faq.component').then((Default) => Default.FaqComponent),
+  },
+  {
+    path: 'news',
+    loadComponent: () => import('./presentation/pages/news/news.component').then((Default) => Default.NewsComponent),
+  },
+  {
     path: '**',
     loadComponent: () =>
       import('./presentation/components/not-found/not-found.component').then((Default) => Default.NotFoundComponent),
