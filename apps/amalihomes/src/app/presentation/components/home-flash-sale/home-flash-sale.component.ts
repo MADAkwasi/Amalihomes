3554afbb7bc01662ba69928b8f27962c8b1d3b95
quavo19 <donald.akite@amalitech.com>
2025-04-09 11:04:31 +0000

    Create story for flashsale

--- apps/amalihomes/src/app/presentation/components/home-flash-sale/home-flash-sale.component.ts
+++ apps/amalihomes/src/app/presentation/components/home-flash-sale/home-flash-sale.component.ts
@@ -1,17 +1,21 @@
-import { Component, inject, ChangeDetectionStrategy } from '@angular/core';
+import { Component, inject, ChangeDetectionStrategy, computed, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { ApplicationStore } from '../../../logic/stores';
 import { ButtonComponent, TextDirective } from '@amalihomes/shared';
 import { selectHomePageSectionData } from '../../../logic/stores/selectors/home-page';
+import { FlashSaleStoryblok } from '../../../types';
 
 @Component({
   selector: 'app-home-flash-sale',
+  standalone: true,
   imports: [CommonModule, ButtonComponent, TextDirective],
   templateUrl: './home-flash-sale.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class HomeFlashSaleComponent {
   private readonly store = inject(Store<ApplicationStore>);
-  protected readonly data = this.store.selectSignal(selectHomePageSectionData('flash-sale'));
+  protected readonly externalData = input<FlashSaleStoryblok | null>(null);
+  private readonly storeData = this.store.selectSignal(selectHomePageSectionData('flash-sale'));
+  protected readonly data = computed(() => this.externalData() ?? this.storeData());
 }
