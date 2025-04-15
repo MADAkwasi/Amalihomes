5926bcbbaa58c61ebf0fabae0c3e3a1406f925f6
Darko Michael Agyeman <111093355+MADAkwasi@users.noreply.github.com>
2025-04-15 12:31:25 +0000

    feat: add fix routing behaviour (#56)

--- apps/amalihomes/src/app/app.component.ts
+++ apps/amalihomes/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, inject, OnInit, PLATFORM_ID } from '@angular/core';
+import { NavigationEnd, Router, RouterModule } from '@angular/router';
 import { RootLayoutComponent } from './presentation/layouts/root-layout/root-layout.component';
+import { filter } from 'rxjs';
+import { InteractionsService } from './logic/services/interactions/interactions.service';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   imports: [RouterModule, RootLayoutComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  private readonly router = inject(Router);
+  private readonly interactionsService = inject(InteractionsService);
+  private readonly platformID = inject(PLATFORM_ID);
+
+  ngOnInit(): void {
+    if (isPlatformBrowser(this.platformID)) {
+      this.router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe(() => {
+        window.scrollTo(0, 0);
+      });
+    }
+
+    this.interactionsService.listenToRouteChange();
+  }
+}
