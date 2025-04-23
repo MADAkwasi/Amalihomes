733c43a2ae26cb775f36c31336d78bc5ba600696
quavo19 <donald.akite@amalitech.com>
2025-04-23 16:51:13 +0000

    feature: Add a section for statistics

---
+++ apps/amalihomes/src/app/presentation/components/about/statistics/statistics.component.ts
@@ -0,0 +1,24 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ResponsiveHeadingComponent } from '../responsive-heading/responsive-heading.component';
+
+interface StatItem {
+  value: string;
+  label: string;
+}
+
+@Component({
+  selector: 'app-statistics',
+  templateUrl: './statistics.component.html',
+  imports: [CommonModule, ResponsiveHeadingComponent],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class StatisticsComponent {
+  // this is just a mocked data, in the future we will get this data from storyblok
+  stats: StatItem[] = [
+    { value: '30+', label: 'Years of Excellence' },
+    { value: '15,000+', label: 'Clients' },
+    { value: '12', label: 'Countries' },
+    { value: '98%', label: 'Satisfied clients' },
+  ];
+}
