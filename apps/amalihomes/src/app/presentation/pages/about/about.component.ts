5dc1d3a297d28adf93535611a0922b9d116c1768
quavo19 <donald.akite@amalitech.com>
2025-04-24 15:22:04 +0000

    Feature: update about page with new section

--- apps/amalihomes/src/app/presentation/pages/about/about.component.ts
+++ apps/amalihomes/src/app/presentation/pages/about/about.component.ts
@@ -2,11 +2,14 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { StatisticsComponent } from '../../components/about/statistics/statistics.component';
 import { FeaturedComponent } from '../../components/about/featured/featured.component';
+import { LocationMapComponent } from '../../components/about/location-map/location-map.component';
+import { LeadershipComponent } from '../../components/about/leadership/leadership.component';
 
 @Component({
   selector: 'app-about',
-  imports: [CommonModule, StatisticsComponent, FeaturedComponent],
+  imports: [CommonModule, StatisticsComponent, FeaturedComponent, LocationMapComponent, LeadershipComponent],
   templateUrl: './about.component.html',
-  styleUrl: './about.component.css',
 })
-export class AboutComponent {}
+export class AboutComponent {
+  public location = 'Amalitech, Ghana';
+}
