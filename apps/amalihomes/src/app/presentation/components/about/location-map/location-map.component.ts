057dba04340fe523b000b88b64246f5c8d18aa88
quavo19 <donald.akite@amalitech.com>
2025-04-24 15:21:22 +0000

    Feature: Add location section with map

---
+++ apps/amalihomes/src/app/presentation/components/about/location-map/location-map.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, input } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
+import { ResponsiveHeadingComponent } from '../responsive-heading/responsive-heading.component';
+
+@Component({
+  selector: 'app-location-map',
+  templateUrl: './location-map.component.html',
+  imports: [ResponsiveHeadingComponent],
+})
+export class LocationMapComponent implements OnInit {
+  public location = input.required<string>();
+  public mapUrl: SafeResourceUrl | undefined;
+
+  // eslint-disable-next-line no-empty-function
+  constructor(private sanitizer: DomSanitizer) {}
+
+  ngOnInit() {
+    const url = `https://www.google.com/maps/embed/v1/place?key=AIzaSyCbJlpT4r7r2EH_eCBG7_sF6IwdFs7PwMI&q=${encodeURIComponent(
+      this.location(),
+    )}`;
+    this.mapUrl = this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  }
+}
