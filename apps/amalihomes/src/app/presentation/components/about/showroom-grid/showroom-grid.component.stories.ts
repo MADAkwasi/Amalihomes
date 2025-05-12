8090261eb262c7536c4029d8632912895fead228
quavo19 <donald.akite@amalitech.com>
2025-05-12 12:38:10 +0000

    refactor: simplify showroom grid story by removing unused args and integrating mock store

--- apps/amalihomes/src/app/presentation/components/about/showroom-grid/showroom-grid.component.stories.ts
+++ apps/amalihomes/src/app/presentation/components/about/showroom-grid/showroom-grid.component.stories.ts
@@ -1,58 +1,32 @@
-import { Meta, StoryObj } from '@storybook/angular';
+import { Meta, StoryObj, moduleMetadata } from '@storybook/angular';
 import { ShowroomGridComponent } from './showroom-grid.component';
-import { action } from '@storybook/addon-actions';
-import { showroomsData } from 'apps/amalihomes/src/app/logic/data/constants/about';
+import { provideMockStore } from '@ngrx/store/testing';
+import { mockedStore } from 'apps/amalihomes/src/app/logic/data/testing/mocked-data';
+
+const showroomGridData = mockedStore.content.body.find((item) => item.component === 'show_room');
 
 const meta: Meta<ShowroomGridComponent> = {
   title: 'Showroom Grid',
   component: ShowroomGridComponent,
   tags: ['autodocs'],
-
-  argTypes: {
-    showroomsTitle: {
-      control: 'text',
-      description: 'Title of the showroom grid',
-      defaultValue: showroomsData.title,
-    },
-    showroomsDescription: {
-      control: 'text',
-      description: 'Description of the showroom grid',
-      defaultValue: showroomsData.description,
-    },
-  },
-  render: (args) => ({
-    props: {
-      ...args,
-      regionChange: action('regionChange'),
-      showroomSelected: action('showroomSelected'),
-    },
-  }),
+  decorators: [
+    moduleMetadata({
+      providers: [
+        provideMockStore({
+          initialState: {
+            storyblokPage: {
+              content: {
+                body: [showroomGridData],
+              },
+            },
+          },
+        }),
+      ],
+    }),
+  ],
 };
 
 export default meta;
 type Story = StoryObj<ShowroomGridComponent>;
 
-export const Default: Story = {
-  args: {
-    showroomsData: showroomsData.data,
-  },
-};
-
-export const WithEmptyRegions: Story = {
-  args: {
-    showroomsData: {
-      Africa: [],
-      Europe: [],
-      'North America': [],
-      'South America': [],
-    },
-  },
-};
-
-export const WithSingleRegion: Story = {
-  args: {
-    showroomsData: {
-      Europe: showroomsData.data['Europe'],
-    },
-  },
-};
+export const Default: Story = {};
