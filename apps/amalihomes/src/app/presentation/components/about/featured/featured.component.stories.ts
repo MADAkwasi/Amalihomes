5c9d3798d7797b2b6250e63e8931d676b6976bf2
quavo19 <donald.akite@amalitech.com>
2025-05-12 12:30:21 +0000

    refactor: simplify featured component story by removing unused args and integrating mock store

--- apps/amalihomes/src/app/presentation/components/about/featured/featured.component.stories.ts
+++ apps/amalihomes/src/app/presentation/components/about/featured/featured.component.stories.ts
@@ -1,39 +1,32 @@
-import { Meta, StoryObj } from '@storybook/angular';
+import { Meta, moduleMetadata, StoryObj } from '@storybook/angular';
 import { FeaturedComponent } from './featured.component';
-import { featuredData } from 'apps/amalihomes/src/app/logic/data/constants/about';
+import { provideMockStore } from '@ngrx/store/testing';
+import { mockedStore } from 'apps/amalihomes/src/app/logic/data/testing/mocked-data';
+
+const meritData = mockedStore.content.body.find((item) => item.component === 'merits');
 
 const meta: Meta<FeaturedComponent> = {
   title: 'Featured',
   component: FeaturedComponent,
   tags: ['autodocs'],
-  argTypes: {
-    data: {
-      control: 'object',
-      description: 'Array of leaders to display',
-    },
-    title: {
-      control: 'text',
-      description: 'Title of the leadership section',
-    },
-    description: {
-      control: 'text',
-      description: 'Description of the leadership section',
-    },
-    image: {
-      control: 'text',
-      description: 'Image URL for the leadership section',
-    },
-  },
+  decorators: [
+    moduleMetadata({
+      providers: [
+        provideMockStore({
+          initialState: {
+            storyblokPage: {
+              content: {
+                body: [meritData],
+              },
+            },
+          },
+        }),
+      ],
+    }),
+  ],
 };
 
 export default meta;
 type Story = StoryObj<FeaturedComponent>;
 
-export const Default: Story = {
-  args: {
-    data: featuredData.data.slice(0, 2),
-    title: 'Global Presence',
-    description: 'Meet our leadership team, who are dedicated to driving our mission and vision forward.',
-    image: 'https://i.postimg.cc/vBqph8zV/image.png',
-  },
-};
+export const Default: Story = {};
