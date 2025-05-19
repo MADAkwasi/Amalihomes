c316e98e5eb4e1505ccc6823960b976fd9dfd10b
quavo19 <donaldakite27@gmail.com>
2025-05-19 19:19:33 +0000

    feat: include AuthState in ApplicationStore interface

--- apps/amalihomes/src/app/logic/stores/index.ts
+++ apps/amalihomes/src/app/logic/stores/index.ts
@@ -2,12 +2,13 @@ import { dummyData } from './mocked-data';
 import { storyblokPageReducer, StoryblokPageState } from './reducers/storyblok.reducers.';
 import { applicationDummyDataReducers } from './reducers/image-data';
 import { interactionsReducer, InteractionsState } from './reducers/interactions.reducer';
-import { authReducer } from './reducers/auth.reducer';
+import { authReducer, AuthState } from './reducers/auth.reducer';
 
 export interface ApplicationStore {
-  dummyData: typeof dummyData; //reducer for dummy data. to be remove in the future
+  dummyData: typeof dummyData;
   storyblokPage: StoryblokPageState;
   interactions: InteractionsState;
+  auth: AuthState;
 }
 
 export const selectApplicationStore = (store: ApplicationStore) => store;
