231e672bfacd5036826ba549e3cdf7b262091c8f
quavo19 <donaldakite27@gmail.com>
2025-05-20 15:12:10 +0000

    refactor: update import path for AuthState in auth.selector.ts

--- apps/amalihomes/src/app/logic/stores/selectors/auth.selector.ts
+++ apps/amalihomes/src/app/logic/stores/selectors/auth.selector.ts
@@ -1,5 +1,5 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
-import { AuthState } from '../reducers/auth.reducer';
+import { AuthState } from '../../../types/auth';
 
 const selectAuthenticatedUserState = createFeatureSelector<AuthState>('auth');
 
