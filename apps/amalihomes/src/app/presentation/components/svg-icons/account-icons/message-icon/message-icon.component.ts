aeec7d754f9e55b7b75f799e2022b736d96cf85e
quavo19 <donaldakite27@gmail.com>
2025-05-21 16:29:33 +0000

    feat(message-icon): enable OnPush change detection strategy for MessageIconComponent

--- apps/amalihomes/src/app/presentation/components/svg-icons/account-icons/message-icon/message-icon.component.ts
+++ apps/amalihomes/src/app/presentation/components/svg-icons/account-icons/message-icon/message-icon.component.ts
@@ -1,10 +1,11 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-message-icon',
   imports: [CommonModule],
   templateUrl: './message-icon.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessageIconComponent {
   public selected = input(false);
