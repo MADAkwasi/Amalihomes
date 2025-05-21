9d48b33af7d734f6500f21d50b1440377d870cb7
quavo19 <donaldakite27@gmail.com>
2025-05-21 16:29:17 +0000

    feat(overview-icon): enable OnPush change detection strategy for OverviewIconComponent

--- apps/amalihomes/src/app/presentation/components/svg-icons/account-icons/overview-icon/overview-icon.component.ts
+++ apps/amalihomes/src/app/presentation/components/svg-icons/account-icons/overview-icon/overview-icon.component.ts
@@ -1,10 +1,11 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-overview-icon',
   imports: [CommonModule],
   templateUrl: './overview-icon.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class OverviewIconComponent {
   public selected = input(false);
