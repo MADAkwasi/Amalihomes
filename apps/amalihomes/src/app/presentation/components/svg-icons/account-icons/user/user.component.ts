8bac09b4b724912b737a8215618d2d30230423f4
quavo19 <donaldakite27@gmail.com>
2025-05-21 16:29:12 +0000

    feat(user-icon): enable OnPush change detection strategy for UserComponent

--- apps/amalihomes/src/app/presentation/components/svg-icons/account-icons/user/user.component.ts
+++ apps/amalihomes/src/app/presentation/components/svg-icons/account-icons/user/user.component.ts
@@ -1,10 +1,11 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-user-icon',
   imports: [CommonModule],
   templateUrl: './user.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class UserComponent {
   public selected = input(false);
