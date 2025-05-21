b37706df5639c3734e4ffddaba4da97e9a1217d1
quavo19 <donaldakite27@gmail.com>
2025-05-21 13:41:13 +0000

    feat(header): implement header component with user authentication display

---
+++ apps/amalihomes/src/app/presentation/components/account/header/header.component.ts
@@ -0,0 +1,23 @@
+import { Component, inject, computed } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BreadcrumbComponent } from '../breadcrumb/breadcrumb.component';
+import { TextDirective } from '@amalihomes/shared';
+import { selectUserAuthenticationState } from 'apps/amalihomes/src/app/logic/stores/selectors/auth.selector';
+import { Store } from '@ngrx/store';
+
+@Component({
+  selector: 'app-header',
+  imports: [CommonModule, BreadcrumbComponent, TextDirective],
+  templateUrl: './header.component.html',
+})
+export class HeaderComponent {
+  private store = inject(Store);
+  protected authenticatedUser = this.store.selectSignal(selectUserAuthenticationState);
+  protected userName = computed(() => {
+    const fullName = this.authenticatedUser()?.user?.full_name;
+    if (!fullName) return 'Guest';
+
+    const firstName = fullName.trim().split(' ')[0];
+    return firstName.charAt(0).toUpperCase() + firstName.slice(1).toLowerCase();
+  });
+}
