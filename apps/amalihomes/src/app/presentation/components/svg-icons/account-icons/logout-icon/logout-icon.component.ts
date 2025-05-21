f0657782e461ae027fa919bd0d5a10987cef8118
quavo19 <donaldakite27@gmail.com>
2025-05-21 16:29:39 +0000

    feat(logout-icon): enable OnPush change detection strategy for LogoutIconComponent

--- apps/amalihomes/src/app/presentation/components/svg-icons/account-icons/logout-icon/logout-icon.component.ts
+++ apps/amalihomes/src/app/presentation/components/svg-icons/account-icons/logout-icon/logout-icon.component.ts
@@ -1,10 +1,11 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-logout-icon',
   imports: [CommonModule],
   templateUrl: './logout-icon.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LogoutIconComponent {
   public selected = input(false);
