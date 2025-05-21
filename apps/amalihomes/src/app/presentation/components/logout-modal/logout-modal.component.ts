7624dedd297b9349347912f840d2d31a8e735c56
quavo19 <donaldakite27@gmail.com>
2025-05-21 10:30:37 +0000

    fix: add public access modifiers to methods in LogoutModalComponent

--- apps/amalihomes/src/app/presentation/components/logout-modal/logout-modal.component.ts
+++ apps/amalihomes/src/app/presentation/components/logout-modal/logout-modal.component.ts
@@ -1,25 +1,26 @@
 import { ButtonComponent } from '@amalihomes/shared';
-import { Component, signal, output } from '@angular/core';
+import { Component, signal, output, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-logout-modal',
   standalone: true,
   imports: [ButtonComponent],
   templateUrl: './logout-modal.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LogoutModalComponent {
   public show = signal(false);
   public readonly confirmed = output<void>();
 
-  open() {
+  public open() {
     this.show.set(true);
   }
 
-  close() {
+  public close() {
     this.show.set(false);
   }
 
-  confirmLogout() {
+  public confirmLogout() {
     this.confirmed.emit();
     this.close();
   }
