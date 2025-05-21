febc4d8b47db35d11e1724729c598e219d7b07d9
quavo19 <donaldakite27@gmail.com>
2025-05-21 16:29:43 +0000

    feat(address-icon): enable OnPush change detection strategy for AddressIconComponent

--- apps/amalihomes/src/app/presentation/components/svg-icons/account-icons/address-icon/address-icon.component.ts
+++ apps/amalihomes/src/app/presentation/components/svg-icons/account-icons/address-icon/address-icon.component.ts
@@ -1,10 +1,11 @@
-import { Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-address-icon',
   imports: [CommonModule],
   templateUrl: './address-icon.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AddressIconComponent {
   public selected = input(false);
