51d6b6445b476452d62b52ab642aaf1698ada589
quavo19 <donaldakite27@gmail.com>
2025-05-21 04:14:50 +0000

    feat: add logout modal component with open/close functionality

---
+++ apps/amalihomes/src/app/presentation/components/logout-modal/logout-modal.component.ts
@@ -0,0 +1,26 @@
+import { ButtonComponent } from '@amalihomes/shared';
+import { Component, signal, output } from '@angular/core';
+
+@Component({
+  selector: 'app-logout-modal',
+  standalone: true,
+  imports: [ButtonComponent],
+  templateUrl: './logout-modal.component.html',
+})
+export class LogoutModalComponent {
+  show = signal(false);
+  public readonly confirmed = output<void>();
+
+  open() {
+    this.show.set(true);
+  }
+
+  close() {
+    this.show.set(false);
+  }
+
+  confirmLogout() {
+    this.confirmed.emit();
+    this.close();
+  }
+}
