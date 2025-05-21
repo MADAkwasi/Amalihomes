15419dddc7ab89bb9878083a220f2fe7250e4fc7
quavo19 <donaldakite27@gmail.com>
2025-05-21 13:55:09 +0000

    feat(logout-icon): create LogoutIconComponent with initial setup

---
+++ apps/amalihomes/src/app/presentation/components/svg-icons/account-icons/logout-icon/logout-icon.component.ts
@@ -0,0 +1,11 @@
+import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-logout-icon',
+  imports: [CommonModule],
+  templateUrl: './logout-icon.component.html',
+})
+export class LogoutIconComponent {
+  public selected = input(false);
+}
