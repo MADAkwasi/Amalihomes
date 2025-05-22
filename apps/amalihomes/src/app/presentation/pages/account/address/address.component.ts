7e1934f597fb98c62c53707164ef0d8c80b0137c
quavo19 <donaldakite27@gmail.com>
2025-05-22 13:16:42 +0000

    feat(address): create AddressComponent with template and CommonModule import

---
+++ apps/amalihomes/src/app/presentation/pages/account/address/address.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-address',
+  imports: [CommonModule],
+  templateUrl: './address.component.html',
+})
+export class AddressComponent {}
